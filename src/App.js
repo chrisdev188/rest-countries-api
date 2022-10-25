b406bb2e4a4be70034f4e2e3d8c865bd90e9aa7b
Chris Bui <chrisbui188.dev@gmail.com>
2022-10-25 13:52:42 -0400

    Configure font, darkmode, lightmode in config file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-3xl font-bold">App</div>;
+  return <React.Fragment>Awesome app</React.Fragment>;
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-dm-elements": "hsl(209, 23%, 22%)",
+        "brand-dm-bg": "hsl(207, 26%, 17%)",
+        "brand-lm-text": "hsl(200, 15%, 8%)",
+        "brand-lm-input": "hsl(0, 0%, 52%)",
+        "brand-lm-bg": "hsl(0, 0%, 98%)",
+        "brand-white": "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
