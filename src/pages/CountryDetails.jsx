fbc2ff3ed546bca25fcf6155741317f08522d231
Chris Bui <chrisbui188.dev@gmail.com>
2022-11-12 11:03:22 -0500

    Transform borders

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -7,11 +7,6 @@ const CountryDetails = ({ countries }) => {
   const { countryName } = useParams();
   const navigate = useNavigate();
 
-  const backToHome = () => {
-    const path = "/";
-    navigate(path);
-  };
-
   const country = useMemo(() => {
     return countries.find(
       (country) =>
@@ -19,87 +14,92 @@ const CountryDetails = ({ countries }) => {
     );
   }, [countryName, countries]);
 
-  const borders = (
-    <div className="flex md:flex-col gap-4 lg:col-span-2 flex-wrap">
-      Borders Countries:
-      <ul className="flex gap-2 flex-wrap">
-        {country.borders?.map((border) => (
-          <li key={border}>
-            <Link
-              className="px-4 py-2 dark:darkmode-element lightmode-element transition-element rounded-sm shadow-surrounding inline-block"
-              to={`/${formatWordSeparatedByHyphen(
-                countries.find((country) => country.cca3 === border).name.common
-              )}`}
-            >
-              {countries.find((country) => country.cca3 === border).name.common}
-            </Link>
-          </li>
-        )) || "There are no borders"}
-      </ul>
-    </div>
-  );
-
-  const backButton = (
+  const BackButton = () => (
     <button
       className="px-6 py-2 shadow-surrounding rounded-sm flex gap-2 items-center mb-20 dark:darkmode-element lightmode-element transition-element"
-      onClick={backToHome}
+      onClick={() => {
+        const path = "/";
+        navigate(path);
+      }}
     >
       <FaArrowLeft /> <span>Back To Home</span>
     </button>
   );
 
-  const flag = (
+  const Flag = ({ country }) => (
     <div className="md:flex-1">
       <img src={country.flags.svg} alt={country.name.common} />
     </div>
   );
 
-  const countryCommonName = (
-    <h2 className="font-bold text-3xl">{country.name.common}</h2>
-  );
-
-  const mainInfo = (
-    <div>
-      <p>Native name: {Object.values(country.name.nativeName)[0].common}</p>
-      <p>Population: {numberToLocaleString(country.population)}</p>
-      <p>Region: {country.region}</p>
-      <p>Sub Region: {country.subregion}</p>
-      <p>Capital: {country.capital}</p>
+  const Information = ({ country }) => (
+    <div className="dark:text-brand-white text-brand-lm-text">
+      <h2 className="font-bold text-3xl">{country.name.common}</h2>
+      <div className="grid grid-cols-1 gap-12 mt-6 lg:grid-cols-2">
+        <div>
+          <p>Native name: {Object.values(country.name.nativeName)[0].common}</p>
+          <p>Population: {numberToLocaleString(country.population)}</p>
+          <p>Region: {country.region}</p>
+          <p>Sub Region: {country.subregion}</p>
+          <p>Capital: {country.capital}</p>
+        </div>
+        <div>
+          <p>Top Level Domain: {country.tld.join(", ")}</p>
+          <p>
+            Currencies:{" "}
+            {Object.values(country.currencies)
+              .map((c) => c.name)
+              .join(", ")}
+          </p>
+          <p>
+            Languages:{" "}
+            {Object.values(country.languages)
+              .map((l) => l)
+              .join(", ")}
+          </p>
+        </div>
+      </div>
     </div>
   );
 
-  const otherInfo = (
-    <div>
-      <p>Top Level Domain: {country.tld.join(", ")}</p>
-      <p>
-        Currencies:{" "}
-        {Object.values(country.currencies)
-          .map((c) => c.name)
-          .join(", ")}
-      </p>
-      <p>
-        Languages:{" "}
-        {Object.values(country.languages)
-          .map((l) => l)
-          .join(", ")}
-      </p>
-    </div>
-  );
+  const BorderList = ({ country, countries }) => {
+    const findCountryWithBorder = (border) => {
+      return countries.find((c) => c.cca3 === border);
+    };
+    return (
+      <div className="flex md:flex-col gap-6 lg:col-span-2 flex-wrap mt-16">
+        <h3 className="text-lg font-semibold text-brand-lm-text dark:text-brand-white">
+          Borders Countries:
+        </h3>
+        <ul className="flex gap-4 flex-wrap">
+          {country.borders?.map((border) => (
+            <li key={border}>
+              <Link
+                className="px-4 py-6 dark:darkmode-element lightmode-element transition-element rounded-sm shadow-surrounding block w-[10rem] h-full"
+                to={`/${formatWordSeparatedByHyphen(
+                  findCountryWithBorder(border).name.common
+                )}`}
+              >
+                <img src={findCountryWithBorder(border).flags.png} alt="" />
+                <h3 className="mt-4">
+                  {findCountryWithBorder(border).name?.common}
+                </h3>
+              </Link>
+            </li>
+          )) || "There are no borders"}
+        </ul>
+      </div>
+    );
+  };
 
   return (
     <div className="md:p-12 py-6 px-4">
-      {backButton}
+      <BackButton />
       <div className="grid md:grid-cols-2 gap-20 items-center">
-        {flag}
-        <div className="dark:text-brand-white text-brand-lm-text">
-          {countryCommonName}
-          <div className="grid grid-cols-1 gap-12 mt-6 lg:grid-cols-2">
-            {mainInfo}
-            {otherInfo}
-            {borders}
-          </div>
-        </div>
+        <Flag country={country} />
+        <Information country={country} />
       </div>
+      <BorderList country={country} countries={countries} />
     </div>
   );
 };
